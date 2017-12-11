e2466bdb6e26e808b8115e27e88392e5baa7d2c1
Trond A Ekseth <troeks@gmail.com>
2017-12-11 10:39:23 +0100

    Replace {get,put}_change to update_change

--- lib/util_changeset.ex
+++ lib/util_changeset.ex
@@ -1,10 +1,7 @@
 defmodule OMTT.Util.Changeset do
-  import Ecto.Changeset, only: [get_field: 2, put_change: 3]
+  import Ecto.Changeset, only: [update_change: 3]
 
   def lowercase_field(changeset, fieldname) when is_atom(fieldname) do
-    case get_field(changeset, fieldname) do
-      nil   -> changeset
-      field -> put_change(changeset, fieldname, String.downcase(field))
-    end
+    update_change(changeset, fieldname, &String.downcase/1)
   end
 end
