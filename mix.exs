91df66efe5a3061904b79942c37cf4d2abb18679
Trond A Ekseth <troeks@gmail.com>
2017-12-11 11:03:03 +0100

    Bump version to 1.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OMTT.Util.Mixfile do
 
   def project do
     [app: :omtt_util,
-     version: "1.1.1",
+     version: "1.1.2",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
